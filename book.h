c23117b6fa57801e116746d0c50e498abf491504
akbarazy <muh.akbar.alfarizy@gmail.com>
2026-02-27 23:30:38 +0700

    Create enum that represent book field

--- book.h
+++ book.h
@@ -3,8 +3,7 @@
 
 #include <stdbool.h>
 
-typedef struct
-{
+typedef struct {
     int id;
     char title[256];
     char author[256];
@@ -12,19 +11,24 @@ typedef struct
     bool available;
 } Book;
 
-typedef struct Node
-{
+typedef struct Node {
     Book book;
     struct Node *next;
     struct Node *prev;
 } Node;
 
-typedef struct
-{
+typedef struct {
     char notification[256];
     bool exitMenu;
 } Output;
 
+typedef enum {
+    ID,
+    TITLE,
+    AUTHOR,
+    YEAR,
+    AVAILABLE
+} Field;
 
 Output addBooks(Node **firstNode);
 Output showBooks(Node *firstNode);
